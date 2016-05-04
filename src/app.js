cc21d88e8aed0db628fe732f3719a3cab440181e
prashobh.chandrabose <prashobh.chandrabose.x@visualiq.com>
2016-05-04 17:58:10 +0530

    $interval implemented

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 "use script";
 angular.module('movieCenter', []);
 angular.module('movieCenter').controller('MainController', ['$scope', '$interval', function ($scope, $interval) {
-        var item = ["banana", "apple", "peach", "orange", "plum"];
+        var items = ["Movie1", "Movie2", "Movie3", "Movie4", "Movie5"];
         $scope.movieIndex = null;
         $scope.currentMovie = "";
-        
-        $scope.getItem = function(){
-            $scope.currentItem = item[$scope.itemIndex];
+
+        $scope.getItem = function () {
+            $scope.currentMovie = items[$scope.movieIndex];
         };
+
+        $interval(function () {
+            $scope.movieIndex = Math.round(Math.random() * (items.length - 1));
+            $scope.getItem();
+        }, 2000);
     }]);
 
